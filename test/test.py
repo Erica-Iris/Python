777670809278c251fbf1bdcc69dd3867f3bee0de
wujie462 <ediath462@gmail.com>
2021-02-07 20:42:00 +0800

    nothing changed

--- test/test.py
+++ test/test.py
@@ -1,25 +1,7 @@
-# # -*- coding:UTF-8 -*-
-# from bs4 import BeautifulSoup
-# import requests
-# import re
+import pygame
 
-# h = {
-#      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
-# }
+pygame.init()
 
-# if __name__ == "__main__":
-#      target = 'https://www.feisuzw.com/book/search.aspx?SearchKey=%B8%B2%CA%D6&SearchClass=1&SeaButton='
-#      req = requests.get(url = target, headers=h)
-#      soup = BeautifulSoup(req.text, 'html.parser')
-#      # urls = soup.select('#CListTitle > a:nth-child(1)')
-#      # Book_Names = soup.select('#CListTitle > a:nth-child(1) > b')
+w,h=500,500
+pygame.display.set_mode((w,h))
 
-#      # for i in urls:
-#      #      url = 'https://www.feisuzw.com' + i.get('href')
-#      # for j in Book_Names:
-#      #      Book_Name = j.get_text
-#      #      print(Book_Name)
-#      # for i in soup.find_all('div'):
-#      #      print(i)
-#      print(soup.prettify)
-print("aaa")
